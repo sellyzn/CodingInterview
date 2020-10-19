74e4c1889cbedba596600e0a04acfae285f8f93f
Ning Zhang <sellyzhang@outlook.com>
2020-10-19 13:56:34 -0700

    add Linkedlist, Two pointers, and array

---
+++ CodingInterview/CodingInterview.Tests/LinkedListTests1.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace CodingInterview.Tests
+{
+    public class LinkedListTests1
+    {
+        [Fact]
+        //public void GetKthFromEndTest()
+        //{
+        //    //arrange
+
+        //    //act
+
+        //    //assert
+
+
+        //}
+    }
+}
---
+++ CodingInterview/CodingInterview/Array/FindRepeatNumber03.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CodingInterview.Array
+{
+    public class FindRepeatNumber03
+    {
+        public int findRepeatNumber(int[] nums)
+        {
+            var set = new HashSet<int>();
+            foreach(var num in nums)
+            {
+                if (set.Contains(num))
+                    return num;
+                else
+                    set.Add(num);
+            }
+            return -1;
+        }
+
+        public int findRepeatNumberrr(int[] nums)
+        {
+            int temp;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                while (nums[i] != i)
+                {
+                    if (nums[i] == nums[nums[i]])
+                    {
+                        return nums[i];
+                    }
+                    temp = nums[i];
+                    nums[i] = nums[temp];
+                    nums[temp] = temp;
+                }
+            }
+            return -1;
+        }
+    }
+}
