116bc9fdca38b1f1185f92297cb64e3b5508901e
Ning Zhang <sellyzhang@outlook.com>
2020-10-26 05:20:51 -0700

    update

--- CodingInterview/CodingInterview/Tree/KthLargest54.cs
+++ CodingInterview/CodingInterview/Tree/KthLargest54.cs
@@ -6,6 +6,18 @@ namespace CodingInterview.Tree
 {
     public class KthLargest54
     {
-
-    }
+    //    int res;
+    //    public int kthLargest(TreeNode root, int k)
+    //    {
+            
+    //        dfs(root, k);
+    //        return res;
+    //    }
+    //    public void dfs(TreeNode root, int k)
+    //    {
+    //        if (root == null)
+    //            return;
+    //        dfs()
+    //    }
+    //}
 }
